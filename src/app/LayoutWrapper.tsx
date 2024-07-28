6816c629e96cf8495c88260ec689058f9250f695
Gagan Aggarwal <113084015+gaganeducase@users.noreply.github.com>
2024-07-28 12:44:59 +0530

    crud operation todo project complete

--- src/app/LayoutWrapper.tsx
+++ src/app/LayoutWrapper.tsx
@@ -1,61 +1,23 @@
 "use client";
 import React, { useEffect, useState } from "react";
-// import "./index.css";
-// import Sidebar from "@/components/Sidebar";
-// import Navbar from "@/components/Navbar";
 import { usePathname } from "next/navigation";
 import { Toaster } from "react-hot-toast";
 import { Provider } from 'react-redux';
 import { store } from '@/redux/store'
-import { debounce, hideLoader, showLoader } from "@/utils/utility";
-// import { useDispatch, useSelector } from "react-redux";
-// import { handleGetUserDataAdminRequest } from "@/redux/actions-reducers/auth/admin/adminAuth";
+import { hideLoader, showLoader } from "@/utils/utility";
 
 const LayoutWrapper = ({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) => {
-    // const dispatch = useDispatch();
     const pathname = usePathname();
     const [baseUrl, setBaseUrl] = useState("");
-    // const [isLogin, setIsLogin] = useState(false);
-
-    // const debounceHandler = debounce(() => {
-    //     if (typeof window !== "undefined") {
-    //         const isLoggedIn = Boolean(localStorage.getItem("isLoggedIn"));
-    //         const userType = localStorage.getItem("userType");
-    //         if (isLoggedIn === true) {
-    //             if (userType === "admin") {
-    //                 dispatch(handleGetUserDataAdminRequest());
-    //             }
-    //         }
-    //     }
-    // }, 500);
-
-    // useEffect(() => {
-    //     debounceHandler();
-    // }, []);
 
     useEffect(() => {
         if (typeof window !== 'undefined') {
             setBaseUrl(location.origin)
-            // const isLoggedIn = Boolean(localStorage.getItem("isLoggedIn"))
-            // if (isLoggedIn === true) {
-            //     setIsLogin(true)
-            // }
             if (pathname === location.pathname) {
-                //   if (pathname.includes("auth/login") === true || pathname.includes("auth/signup") === true) {
-                //       if (isLoggedIn === true) {
-                //           location.replace("/")
-                //       }
-                //   }
-
-                //   if (pathname === "/") {
-                //       if (isLoggedIn === false) {
-                //           location.replace("/auth/login/client")
-                //       }
-                //   }
                 hideLoader()
             } else {
                 showLoader()
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarMenuItem, NavbarMenu, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
-import { Input } from "@nextui-org/react";
-import { Textarea } from "@nextui-org/input";
+import { Button } from "@nextui-org/react";
+
 import { useDispatch, useSelector } from 'react-redux';
 import { handleGetTodoRequest } from '@/redux/actions-reducers/todo/todo';
 import toast from 'react-hot-toast';
+import NavbarComponent from '@/components/Navbar';
+import AddEditTodoModal from '@/components/AddEditTodoModal';
 
 const Home = () => {
     const dispatch = useDispatch();
     const [flag, setFlag] = useState(false)
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [editTodoLoadingId, setEditTodoLoadingId] = useState("")
     const [deleteTodoLoadingId, setDeleteTodoLoadingId] = useState("")
     const [modalIsOpen, setModalIsOpen] = useState(false)
@@ -24,10 +23,6 @@ const Home = () => {
         description: ""
     })
 
-    const menuItems = [
-        "Add Todo",
-        "Log Out",
-    ];
     const textTruncate = (text: string, number: number) => {
         if (text.length > number) {
             return text.slice(0, number) + '...'
@@ -68,35 +63,7 @@ const Home = () => {
         setLoad(false)
     }
 
-    const handleAddEditTodo = async (payload: any) => {
-        setLoad(true)
-        const response = await fetch(`api/todo`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(payload)
-        })
-        const jsonData = await response.json();
-        if (jsonData.success === true) {
-            toast.success(jsonData.message);
-            setFlag(!flag)
-            setModalIsOpen(!modalIsOpen)
-        } else {
-            toast.error(jsonData.message);
-        }
-        setLoad(false)
-    }
 
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (editTodo) {
-            handleAddEditTodo({ ...todoState, id: editTodo.id })
-        } else {
-            handleAddEditTodo({ ...todoState })
-        }
-    }
 
     const handleDeleteTodo = async (id: string) => {
         setLoad(true)
@@ -121,108 +88,23 @@ const Home = () => {
 
     return (
         <>
-            <Modal isOpen={modalIsOpen} onOpenChange={() => setModalIsOpen(!modalIsOpen)}>
-                <ModalContent>
-                    {(onClose) => (
-                        <>
-                            <ModalHeader className="flex flex-col gap-1">
-                                {editTodo ? "Update" : "Add"} Todo
-                            </ModalHeader>
-                            <form onSubmit={handleSubmit}>
-
-                                <ModalBody>
-                                    <Input
-                                        type="text"
-                                        label="Title"
-                                        required
-                                        onChange={(e) => setTodoState({ ...todoState, title: e.target.value })}
-                                        value={todoState.title}
-                                        placeholder="Enter todo title"
-                                    />
-                                    <Textarea
-                                        label="Description"
-                                        required
-                                        onChange={(e) => setTodoState({ ...todoState, description: e.target.value })}
-                                        value={todoState.description}
-                                        placeholder="Enter your description"
-                                    />
-                                </ModalBody>
-                                <ModalFooter>
-                                    <Button color="danger" variant="light" onPress={onClose}>
-                                        Close
-                                    </Button>
-                                    <Button
-                                        isLoading={load}
-                                        color="primary"
-                                        type='submit'
-                                    >
-                                        {editTodo ? "Update" : "Add"}
-                                    </Button>
-                                </ModalFooter>
-                            </form>
-                        </>
-                    )}
-                </ModalContent>
-            </Modal>
-
-            <Navbar
-                isBordered
-                isMenuOpen={isMenuOpen}
-                onMenuOpenChange={setIsMenuOpen}
-            >
-                <NavbarContent className="sm:hidden" justify="start">
-                    <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
-                </NavbarContent>
-
-                <NavbarContent className="sm:hidden pr-3" justify="center">
-                    <NavbarBrand>
-
-                        <p className="font-bold text-inherit">Todo App</p>
-                    </NavbarBrand>
-                </NavbarContent>
-
-                <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                    <NavbarBrand>
-                        <p className="font-bold text-inherit">Todo App</p>
-                    </NavbarBrand>
-
-                </NavbarContent>
-
-                <NavbarContent justify="end">
-                    <NavbarItem className="hidden sm:flex">
-                        <Link onPress={() => {
-                            setModalIsOpen(!modalIsOpen)
-                            setEditTodo(null)
-                            setTodoState({
-                                title: "",
-                                description: ""
-                            })
-                        }} className='cursor-pointer'>Add Todo</Link>
-                    </NavbarItem>
-                    <NavbarItem className="hidden sm:flex">
-                        <Button as={Link} color="warning" href="#" variant="flat">
-                            Logout
-                        </Button>
-                    </NavbarItem>
-                </NavbarContent>
-
-                <NavbarMenu>
-                    {menuItems.map((item, index) => (
-                        <NavbarMenuItem key={`${item}-${index}`}>
-                            <Link
-                                className="w-full"
-                                color={
-                                    index === menuItems.length - 1 ? "danger" : "warning"
-                                }
-                                href="#"
-                                size="lg"
-                            >
-                                {item}
-                            </Link>
-                        </NavbarMenuItem>
-                    ))}
-                </NavbarMenu>
-            </Navbar>
+            <AddEditTodoModal
+                modalIsOpen={modalIsOpen}
+                setModalIsOpen={setModalIsOpen}
+                editTodo={editTodo}
+                todoState={todoState}
+                setFlag={setFlag}
+                flag={flag}
+                setLoad={setLoad}
+                load={load}
+                setTodoState={setTodoState}
+            />
+            <NavbarComponent
+                modalIsOpen={modalIsOpen}
+                setModalIsOpen={setModalIsOpen}
+                setEditTodo={setEditTodo}
+                setTodoState={setTodoState}
+            />
             <div className=''>
                 <div className="flex flex-wrap justify-center">
                     {todoList.map((item: any, index: number) => (
